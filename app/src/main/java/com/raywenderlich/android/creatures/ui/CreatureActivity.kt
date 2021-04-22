6df6fc79156e8d27b703b87c05f1ae72ffe04e7d
Juan Flores <jgonzalofh2898@gmail.com>
2021-04-22 12:38:07 -0500

    reformat code

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureActivity.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureActivity.kt
@@ -58,62 +58,62 @@ class CreatureActivity : AppCompatActivity() {
         }
     }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_creature)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creature)
 
-      setupCreature()
-      setupTitle()
-      setupViews()
-      setupFavoriteButton()
-      setupFoods()
-  }
-
-  private fun setupCreature() {
-    val creatureById = CreatureStore.getCreatureById(intent.getIntExtra(EXTRA_CREATURE_ID, 1))
-    if (creatureById == null) {
-      Toast.makeText(this, getString(R.string.invalid_creature), Toast.LENGTH_SHORT).show()
-      finish()
-    } else {
-      creature = creatureById
+        setupCreature()
+        setupTitle()
+        setupViews()
+        setupFavoriteButton()
+        setupFoods()
     }
-  }
 
-  private fun setupTitle() {
-    title = String.format(getString(R.string.detail_title_format), creature.nickname)
-    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-  }
-
-  private fun setupViews() {
-    headerImage.setImageResource(resources.getIdentifier(creature.uri, null, packageName))
-    fullName.text = creature.fullName
-    planet.text = creature.planet
-  }
-
-  private fun setupFavoriteButton() {
-    setupFavoriteButtonImage(creature)
-    setupFavoriteButtonClickListener(creature)
-  }
-
-  private fun setupFavoriteButtonImage(creature: Creature) {
-    if (creature.isFavorite) {
-      favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
-    } else {
-      favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
+    private fun setupCreature() {
+        val creatureById = CreatureStore.getCreatureById(intent.getIntExtra(EXTRA_CREATURE_ID, 1))
+        if (creatureById == null) {
+            Toast.makeText(this, getString(R.string.invalid_creature), Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            creature = creatureById
+        }
     }
-  }
 
-  private fun setupFavoriteButtonClickListener(creature: Creature) {
-      favoriteButton.setOnClickListener { _ ->
-          if (creature.isFavorite) {
-              favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
-              Favorites.removeFavorite(creature, this)
-          } else {
-              favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
-              Favorites.addFavorite(creature, this)
-          }
-      }
-  }
+    private fun setupTitle() {
+        title = String.format(getString(R.string.detail_title_format), creature.nickname)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setupViews() {
+        headerImage.setImageResource(resources.getIdentifier(creature.uri, null, packageName))
+        fullName.text = creature.fullName
+        planet.text = creature.planet
+    }
+
+    private fun setupFavoriteButton() {
+        setupFavoriteButtonImage(creature)
+        setupFavoriteButtonClickListener(creature)
+    }
+
+    private fun setupFavoriteButtonImage(creature: Creature) {
+        if (creature.isFavorite) {
+            favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
+        } else {
+            favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
+        }
+    }
+
+    private fun setupFavoriteButtonClickListener(creature: Creature) {
+        favoriteButton.setOnClickListener { _ ->
+            if (creature.isFavorite) {
+                favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
+                Favorites.removeFavorite(creature, this)
+            } else {
+                favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
+                Favorites.addFavorite(creature, this)
+            }
+        }
+    }
 
     private fun setupFoods() {
         foodRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.HORIZONTAL, false)
